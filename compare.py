a34e4de063401f2a237649b84bc74b9ca20a5ee2
Unknown <s6mahapp@uni-bonn.de>
2021-01-18 14:04:22 +0100

    added confusion matrix and capacity

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
 from scipy.stats import wasserstein_distance
+from scipy.stats import entropy
 
 #read sender data (just load once)
 fp_send = open("sen_exec.txt", "r")
@@ -33,15 +34,29 @@ recv  = [0] * (len(send)+100) # longer than send, because sometimes recv has mor
 for y in range(0,8):
     print('Test:',y)
     load_receiver_data(recv, len(send))
-    # calc accuracy
-    sum = 0
+    # calc confusionmatrix
+    tp = 0  # true positives
+    fp = 0  # false positives
+    tn = 0  # true negatives
+    fn = 0  # false negatives
     for x in range(0,len(send)):
-        if recv[x] == send[x]:
-            sum += 1
-    acc = sum/len(send)
+        if send[x] == 1 and recv[x] == 1:
+            tp += 1
+        elif send[x] == 1 and recv[x] == 0:
+            fp += 1
+        elif send[x] == 0 and recv[x] == 0:
+            tn += 1
+        else:
+            fn += 1
+    acc = (tp + tn) / ( tp + fp + tn + fn)
+
+    # calc channel capacity (shannon)
+    cap = 1-entropy([acc,1-acc])
 
     # calc wasserstein distance
     wsd = wasserstein_distance(recv,send)
     print('Wassersteindistance:',wsd)
     print('Accuracy:',acc)
     print('Errorrate:',1-acc)
+    print('TP = ', tp, ' FP = ', fp, ' TN = ', tn, ' FN = ', fn)
+    print('Capacity = ', cap)
